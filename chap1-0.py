182cafd2d98e0617d700889bd640f4d188c6014b
Akiko Eriguchi <erigchi@gmail.com>
2015-04-28 11:08:25 +0900

    Reverse the given strings
    Reverse the given strings

--- chap1-0.py
+++ chap1-0.py
@@ -5,3 +5,25 @@
 00. 文字列の逆順
 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
 """
+
+def reverse(lst):
+    n = len(lst)
+    for i in range(n/2):
+        j      = n-i-1
+        jtem   = lst[j] # 最後の要素
+        lst[j] = lst[i]
+        lst[i] = jtem
+    return lst
+
+def main():
+    mojis    = "stressed"
+    print "before:", mojis
+    
+    moji_lst = list(mojis)     # string -> list 
+    re_lst   = reverse(moji_lst)
+    re_mojis = "".join(re_lst) # list -> string
+
+    print "after: ", re_mojis
+
+if __name__ == "__main__":
+    main()
