49e4c072331ac0e3fd2ce0aa0f47500acc04072b
Akiko Eriguchi <erigchi@gmail.com>
2015-04-29 23:43:07 +0900

    Set a python environment and a coding language:
    1. Set python environment
    2. Set a coding language (utf-8)
    3. Clear a Problem for this code to solve

---
+++ chap1-6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+06. 集合
+"paraparaparadise"と"paragraph"に含まれる文字bi-gramの集合を，それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．さらに，'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
+
+"""
