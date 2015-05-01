99822b07989ed79c4fe06c85148620761cfcdcdf
Akiko Eriguchi <erigchi@gmail.com>
2015-05-01 22:54:17 +0900

    Load a file and count the lines:
    1. Load a file
    2. Count the lines
    3. Use a command, $ wc, to count the lines

--- Chap2/chap2_0.py
+++ Chap2/chap2_0.py
@@ -5,3 +5,22 @@
 10. 行数のカウント
 行数をカウントせよ．確認にはwcコマンドを用いよ．
 """
+
+import subprocess
+
+def main():
+    file = './hightemp.txt'
+    f    = open(file)
+    line = f.readline()
+
+    i = 0    
+    while line:
+        line = f.readline()
+        i += 1
+    print "Python counts: %d" % i 
+
+    print "$ wc %s" % file
+    subprocess.Popen(["wc", file], )
+        
+if __name__ == "__main__":
+    main()
