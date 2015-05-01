55ddc8bd6dcf606e28d7133975100276894e3076
Akiko Eriguchi <erigchi@gmail.com>
2015-05-01 23:26:58 +0900

    Change tab to space in text:
    Change tab to space in text

--- Chap2/chap2_1.py
+++ Chap2/chap2_1.py
@@ -5,3 +5,23 @@
 11. タブをスペースに置換
 タブ1文字につきスペース1文字に置換せよ．確認にはsedコマンド，trコマンド，もしくはexpandコマンドを用いよ．
 """
+
+def main():
+    file = './hightemp.txt'
+    f    = open(file)
+    doc  = []
+    line = f.readline()
+
+    i = 0    
+    while line:
+        text = unicode(line, "utf-8").split("\t")
+        doc.append(" ".join(text))
+        i += 1
+        line = f.readline()
+    f.close()
+    print "Python does: %s" % "".join(doc)
+
+    # $ sed $'s/\t/ /g ./hightemp.txt
+
+if __name__ == "__main__":
+    main()
