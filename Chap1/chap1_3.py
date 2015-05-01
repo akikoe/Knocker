eec3bacb1e9f920e9823f202c6a1ffc50f46c43a
Akiko Eriguchi <erigchi@gmail.com>
2015-05-01 15:58:04 +0900

    Revise the code:
    Output the count number of each word

--- Chap1/chap1_3.py
+++ Chap1/chap1_3.py
@@ -9,27 +9,12 @@
 import collections
 import re
 
-def is_alphabet(term):
-    return re.match("[a-z]|[A-Z]", term)
-
-def count(lst):
-    count_lst = []
-    
-    n = len(lst)
-    for i in range(n):
-        term  = list(lst[i])
-        c_dic = collections.Counter(term)
-        c_lst = [(v, k) for k, v in c_dic.items()]
-        c_lst.sort(reverse = True)
-        count_lst.append([i[1] for i in c_lst if is_alphabet(i[1])])
-    return count_lst
-
 def main():
     text  = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
     print "Text: ", text
     word_lst = text.split()
-    wc_lst = count(word_lst) # list -> list
+    wc_lst = [len(i) for i in word_lst if i.isalpha()] # list -> list
     print "Count: ", wc_lst
         
 
