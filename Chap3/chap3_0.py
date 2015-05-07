68323fbf7f67bc6c4a3255f8513f67986b95f49f
Akiko Eriguchi <erigchi@gmail.com>
2015-05-07 11:40:35 +0900

    Load Json file, extract the text about "UK" and display it:
    1. Load Json file
    2. Extract the text about "UK"
    3. Display it

--- Chap3/chap3_0.py
+++ Chap3/chap3_0.py
@@ -5,3 +5,22 @@
 20. JSONデータの読み込み
 Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
 """
+
+import json
+
+def main():
+
+    f_path = './jawiki-country.json'
+    f = open(f_path, 'r')
+    line = f.readline()
+
+    while line:
+        data = json.loads(line)
+        if data["title"] == u"イギリス":
+            print data["text"] # Chap3-0
+            break
+        line = f.readline()
+        
+if __name__ == "__main__":
+    main()
+
