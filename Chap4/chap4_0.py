5ae5e3496c318d2cca3a5441e25efb12bc4ef701
Akiko Eriguchi <erigchi@gmail.com>
2015-05-29 15:31:00 +0900

    Revise chap4_0.py:
    Chap 4-0 problem requires to make a list of list composed of morpheme dic,
    but there is one list holding all mopheme dics.
    1. Make a list per each sentence.
    2. Append it to one list.
    3. Revise some codes in the other functions using the above list.

--- Chap4/chap4_0.py
+++ Chap4/chap4_0.py
@@ -17,25 +17,30 @@ import re
 形態素解析結果（neko.txt.mecab）を読み込むプログラムを実装せよ．ただし，各形態素は表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）をキーとするマッピング型に格納し，1文を形態素（マッピング型）のリストとして表現せよ．第4章の残りの問題では，ここで作ったプログラムを活用せよ．
 """
 def MakeMorphoDic(path):
-    m_dic_lst   = []
+    lst, m_dic_lst   = [], []
     
     f = open(path, "r")
     line = f.readline()
-    p = re.compile(u"(.*?)\t(.*?)\n")
+    p = re.compile(u"(.*?)\t(.*)")
     while line:
         m_dic = {}
-        m = p.match(line)
-        if m:
-            #Memo: 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
-            m_dic["surf"] = m.group(1) # 表層形
-            feat_lst = m.group(2).split(',')
-            m_dic["base"] = feat_lst[6]
-            m_dic["pos"]  = feat_lst[0]
-            m_dic["pos1"] = feat_lst[1]
-            m_dic_lst.append(m_dic)
+        line = line.rstrip()
+        if line == "EOS":
+            lst.append(m_dic_lst)
+            m_dic_lst = []
+        else:
+            m = p.match(line)
+            if m:
+                #Memo: 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+                m_dic["surf"] = m.group(1) # 表層形
+                feat_lst = m.group(2).split(',')
+                m_dic["base"] = feat_lst[6]
+                m_dic["pos"]  = feat_lst[0]
+                m_dic["pos1"] = feat_lst[1]
+                m_dic_lst.append(m_dic)
         line = f.readline()    
     f.close()
-    return m_dic_lst
+    return lst
 
 """
 31. 動詞
@@ -45,22 +50,24 @@ def MakeMorphoDic(path):
 動詞の原形をすべて抽出せよ．
 """
 def ExtractVerbs(lst):
-    for m_dic in lst:
-        if m_dic["pos"] == "動詞":
-            m_dic["surf"]
-            #print m_dic["surf"]           # chap4_1
-            m_dic["base"]
-            #print m_dic["base"]           # chap4_2
+    for sen in lst:
+        for m_dic in sen:
+            if m_dic["pos"] == "動詞":
+                m_dic["surf"]
+                #print m_dic["surf"]           # chap4_1
+                m_dic["base"]
+                #print m_dic["base"]           # chap4_2
 
 """
 33. サ変名詞
 サ変接続の名詞をすべて抽出せよ．
 """
 def ExtractNouns(lst):
-    for m_dic in lst:
-        if m_dic["pos"] == "名詞" and m_dic["pos1"] == "サ変接続":
-            m_dic["surf"]
-            #print m_dic["surf"] # chap4_3
+    for sen in lst:
+        for m_dic in sen:
+            if m_dic["pos"] == "名詞" and m_dic["pos1"] == "サ変接続":
+                m_dic["surf"]
+                #print m_dic["surf"] # chap4_3
 
 """
 34. 「AのB」
@@ -69,22 +76,24 @@ def ExtractNouns(lst):
 def ExtractNounPhrase(lst):
     flag   = 0
     phrase = ""
-    
-    for m_dic in lst:
-        if flag == 0 and m_dic["pos"] == "名詞":
-            phrase += m_dic["surf"]
-            flag = 1
-        elif flag == 1 and m_dic["surf"] == "の":
-            phrase += m_dic["surf"]
-            flag = 2
-        elif flag == 2 and m_dic["pos"] == "名詞":
-            phrase += m_dic["surf"]
-            print phrase
-            flag = 0
-            phrase = ""
-        else:
-            flag   = 0
-            phrase = ""
+
+    for sen in lst:
+        #for dic1, dic2, dic2 in zip(m_dic[:], m_dic[1:], m_dic[2:]): # とすると短くかける
+        for m_dic in sen:
+            if flag == 0 and m_dic["pos"] == "名詞":
+                phrase += m_dic["surf"]
+                flag = 1
+            elif flag == 1 and m_dic["surf"] == "の":
+                phrase += m_dic["surf"]
+                flag = 2
+            elif flag == 2 and m_dic["pos"] == "名詞":
+                phrase += m_dic["surf"]
+                #print phrase
+                flag = 0
+                phrase = ""
+            else:
+                flag   = 0
+                phrase = ""
             
         
 def main():
