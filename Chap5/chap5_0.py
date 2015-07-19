2626030c857f34af87ba341540635c2212391583
Akiko Eriguchi <johndoe@example.com>
2015-07-19 10:24:27 +0900

    Extract verb phrase frames:
    Extract verb phrase frames using chap4-5

--- Chap5/chap5_0.py
+++ Chap5/chap5_0.py
@@ -196,7 +196,33 @@ def extract_kaku_pattern(lst):
                     if joshi_lst:
                         moji += " ".join(joshi_lst)
                         print moji
-    
+
+"""
+46. 動詞の格フレーム情報の抽出
+45のプログラムを改変し，述語と格パターンに続けて項（述語に係っている文節そのもの）をタブ区切り形式で出力せよ．45の仕様に加えて，以下の仕様を満たすようにせよ．
+
+項は述語に係っている文節の単語列とする（末尾の助詞を取り除く必要はない）
+述語に係る文節が複数あるときは，助詞と同一の基準・順序でスペース区切りで並べる
+「吾輩はここで始めて人間というものを見た」という例文（neko.txt.cabochaの8文目）を考える． この文は「始める」と「見る」の２つの動詞を含み，「始める」に係る文節は「ここで」，「見る」に係る文節は「吾輩は」と「ものを」と解析された場合は，次のような出力になるはずである．
+
+始める  で      ここで
+見る    は を   吾輩は ものを
+"""
+def extract_kaku_VPpattern(lst):
+    for i in lst[7:8]: # 8文目
+        for j in i: # 1文中のchunk
+            moji = ""
+            for k in j.morphs:
+                if k.pos == "動詞":
+                    moji += k.base # 動詞の基本形
+                    joshi_lst, phrase_lst = [], []
+                    for cnum in j.scrs:
+                        morphs = i[cnum].morphs
+                        joshi_lst  += [k.surface for k in morphs
+                                                if k.pos == "助詞"]
+                        phrase_lst += ["".join([k.surface for k in morphs])]
+                    moji += "\t" + " ".join(joshi_lst) + "\t" + " ".join(phrase_lst)
+                    print moji
     
 def main():
     # Chap5_0
@@ -218,6 +244,9 @@ def main():
 
     # Chap5_5
     extract_kaku_pattern(chunk_lst)
+
+    # Chap5_6
+    extract_kaku_VPpattern(chunk_lst)
     
 if __name__ == "__main__":
     main()       
