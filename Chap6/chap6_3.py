0580d1e14603ee31890a083f16a2cb6781d145e6
Akiko Eriguchi <erigchi@gmail.com>
2015-09-06 22:33:03 +0900

    Set a python environment and a coding language:
    1. Set python environment
    2. Set a coding language (utf-8)
    3. Clear a problem for this code to solve

---
+++ Chap6/chap6_3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+第6章: 英語テキストの処理
+
+前準備
+英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
+"""
+
+"""
+53. Tokenization
+Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得よ．また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
+"""
+
+def main():
+
+if __name__ == '__main__':
+    main()
