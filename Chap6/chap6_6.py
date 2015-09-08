ad6a2b560896f2a60db9bc7805c98d7ed1f73b2a
Akiko Eriguchi <erigchi@gmail.com>
2015-09-08 12:30:26 +0900

    Set a python environment and a coding language:
    1. Set python environment
    2. Set a coding language (utf-8)
    3. Clear a problem for this code to solve

---
+++ Chap6/chap6_6.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+第6章: 英語テキストの処理
+
+前準備
+英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
+"""
+
+"""
+56. 共参照解析
+Stanford Core NLPの共参照解析の結果に基づき，文中の参照表現（mention）を代表参照表現（representative mention）に置換せよ．ただし，置換するときは，「代表参照表現（参照表現）」のように，元の参照表現が分かるように配慮せよ．
+"""
+
+def main():
+
+if __name__ == '__main__':
+    main()
