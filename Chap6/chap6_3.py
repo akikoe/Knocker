01f5059776ee47d10316d206880ba885b4f91ba0
Akiko Eriguchi <erigchi@gmail.com>
2015-09-09 01:02:15 +0900

    Revise chap6_3.py:
    Use XML Python module

--- Chap6/chap6_3.py
+++ Chap6/chap6_3.py
@@ -7,27 +7,30 @@
 前準備
 英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
 """
+import xml.etree.ElementTree as ET
 import re
 
 """
 53. Tokenization
 Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得よ．また，このXMLファイルを読み込み，入力テキストを1行1単語の形式で出力せよ．
 $ java -cp stanford-corenlp-3.5.2.jar:stanford-corenlp-3.5.2-models.jar:xom.jar:joda-time.jar:jollyday.jar:ejml-3.5.2.jar -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -file ~/GitHub/Knocker/Chap6/nlp.txt
--> input.txt.xmlファイルが生成される　
+-> nlp.txt.xmlファイルが生成される　
 """
 
-def tokenize_xml(path):
+def read_xml(path):
+    tree = ET.parse(path)
+    return tree
+
+def tokenize_xml(tree):
     wrd_lst = []
-    lst = open(path, 'r').readlines()
-    p = re.compile(r"<word>(.*?)</word>")
-    for line in lst:
-        m = p.match(line.lstrip())
-        if m:
-            wrd_lst.append(m.group(1))
+    root = tree.getroot()
+    for token in root.findall('.//token'):
+        wrd_lst.append(token.find('word').text)
     return wrd_lst
 
 def main():
-    wrd_lst = tokenize_xml("./nlp.txt.xml")
+    tree = read_xml("./nlp.txt.xml")
+    wrd_lst = tokenize_xml(tree)
     print ("\n".join(wrd_lst))
 
 if __name__ == '__main__':
