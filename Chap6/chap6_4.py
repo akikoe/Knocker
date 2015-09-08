147bb3e1dd4121b0651caab89c8e99a247163171
Akiko Eriguchi <erigchi@gmail.com>
2015-09-09 01:11:15 +0900

    Revise chap6_4.py:
    Use XML Python module

--- Chap6/chap6_4.py
+++ Chap6/chap6_4.py
@@ -7,6 +7,8 @@
 前準備
 英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
 """
+
+import chap6_3
 import re
 
 """
@@ -15,29 +17,20 @@ Stanford Core NLPの解析結果XMLを読み込み，単語，レンマ，品詞
 
 """
 
-def tokenize_xml(path):
-    wrd_line = ""
+def tokenize_xml(tree):
     wrd_lst = []
-    lst = open(path, 'r').readlines()
-    wp = re.compile(r"<word>(.*?)</word>")
-    lp = re.compile(r"<lemma>(.*?)</lemma>")
-    pp = re.compile(r"<POS>(.*?)</POS>")
-    for line in lst:
-        wm = wp.match(line.lstrip())
-        lm = lp.match(line.lstrip())
-        pm = pp.match(line.lstrip())
-        if wm:
-            wrd_line += wm.group(1)
-        elif lm:
-            wrd_line += "\t" + lm.group(1)
-        elif pm:
-            wrd_line += "\t" + pm.group(1)
-            wrd_lst.append(wrd_line)
-            wrd_line = ""
+    root = tree.getroot()
+    for token in root.findall('.//token'):
+        wrd = token.find('word').text
+        lem = token.find('lemma').text
+        pos = token.find('POS').text
+        sen = "{}\t{}\t{}".format(wrd, lem, pos)
+        wrd_lst.append(sen)
     return wrd_lst
 
 def main():
-    wrd_lst = tokenize_xml("./nlp.txt.xml")
+    tree = chap6_3.read_xml("./nlp.txt.xml")
+    wrd_lst = tokenize_xml(tree)
     print ("\n".join(wrd_lst))
 
 if __name__ == '__main__':
