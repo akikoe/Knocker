7e8618dd35fbccfc5badc79b65d3974aea54d11f
Akiko Eriguchi <erigchi@gmail.com>
2015-09-08 12:51:39 +0900

    Set a python environment and a coding language:
    1. Set python environment
    2. Set a coding language (utf-8)
    3. Clear a problem for this code to solve

---
+++ Chap6/chap6_7.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+第6章: 英語テキストの処理
+
+前準備
+英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
+"""
+
+"""
+57. 係り受け解析
+Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）を有向グラフとして可視化せよ．可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
+"""
+
+def main():
+
+if __name__ == '__main__':
+    main()
