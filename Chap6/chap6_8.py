d6108ac9e058a4e3165db69881ab08c3707899b8
Akiko Eriguchi <erigchi@gmail.com>
2015-09-09 11:06:40 +0900

    Set a python environment and a coding language:
    1. Set python environment
    2. Set a coding language (utf-8)
    3. Clear a problem for this code to solve

---
+++ Chap6/chap6_8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+第6章: 英語テキストの処理
+
+前準備
+英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
+"""
+
+"""
+58. タプルの抽出
+Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）に基づき，「主語 述語 目的語」の組をタブ区切り形式で出力せよ．ただし，主語，述語，目的語の定義は以下を参考にせよ．
+
+述語: nsubj関係とdobj関係の子（dependant）を持つ単語
+主語: 述語からnsubj関係にある子（dependent）
+目的語: 述語からdobj関係にある子（dependent）
+"""
+
+def main():
+
+if __name__ == '__main__':
+    main()
