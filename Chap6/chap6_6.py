5848d76cfda920b11dfa0373a8a7b66b53593119
Akiko Eriguchi <erigchi@gmail.com>
2015-09-09 10:08:48 +0900

    Revise chap6_6.py:
    Use XML Python module

--- Chap6/chap6_6.py
+++ Chap6/chap6_6.py
@@ -8,6 +8,7 @@
 英語のテキスト（nlp.txt）に対して，以下の処理を実行せよ．
 """
 
+import chap6_3
 import re
 
 """
@@ -15,27 +16,21 @@ import re
 Stanford Core NLPの共参照解析の結果に基づき，文中の参照表現（mention）を代表参照表現（representative mention）に置換せよ．ただし，置換するときは，「代表参照表現（参照表現）」のように，元の参照表現が分かるように配慮せよ．
 """
 
-def coreference(path):
-    cor_word = ""
+def coreference(tree):
     coref_lst = []
-    lst = open(path, 'r').readlines()
-    rp = re.compile(r"<mention representative=\"true\">")
-    tp = re.compile(r"<text>(.*?)</text>")
-    for line in lst:
-        rm = rp.match(line.lstrip())
-        tm = tp.match(line.lstrip())
-        if rm:
-            rep_word = ""
-        if tm:
-            if rep_word:
-                cor_word = tm.group(1)
-                coref_lst.append(rep_word + " ("+ cor_word + ")")
+    root = tree.getroot()
+    for corefer in root.findall('.//coreference'):
+        for mention in corefer.findall('mention'):
+            if mention.get('representative') == "true":
+                rep_word = mention.find('text').text
             else:
-                rep_word = tm.group(1)
+                cor_word = mention.find('text').text
+                coref_lst.append(rep_word + " ("+ cor_word + ")")
     return coref_lst
 
 def main():
-    coref_lst = coreference("./nlp.txt.xml")
+    tree = chap6_3.read_xml("./nlp.txt.xml")
+    coref_lst = coreference(tree)
     print ("\n".join(coref_lst))
 
 if __name__ == '__main__':
